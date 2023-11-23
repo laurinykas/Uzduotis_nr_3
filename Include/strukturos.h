968daeb4c6054a2bec0f22d37c0cf31c0602a39e
laurinykas <laurynasjonikas123@gmail.com>
2023-11-23 13:51:22 +0200

    STRUKTURA -> KLASE

--- Include/strukturos.h
+++ Include/strukturos.h
@@ -7,44 +7,57 @@
 
 #include "bibliotekos.h"
 
-struct Duomenys {
-    string vardas = "";
-    string pavarde = "";
-    vector<double> nd; //namu darbai
-    int egzaminas = 0;
-    float vid = 0;
-    float med = 0;
-    int studentu_kiekis = 0;
-    float galutinis = 0;
+class Duomenys {
+public:
+    Duomenys() : vardas(""), pavarde(""), egzaminas(0), vid(0), med(0), studentu_kiekis(0), galutinis(0) {}
 
-    void Galutinis (int input ){
-        if(input == 1){
-            galutinis = vid ;
-        }
-        else {
-            galutinis = med;
-        }
-
+    void setVardas(const std::string& v) {
+        vardas = v;
     }
 
-    void Vidurkis() {
+    void setPavarde(const std::string& p) {
+        pavarde = p;
+    }
 
+    void setNamai(const std::vector<double>& nd_values) {
+        nd = nd_values;
+    }
+
+    void setEgzaminas(int e) {
+        egzaminas = e;
+    }
+
+    void setStudentuKiekis(int k) {
+        studentu_kiekis = k;
+    }
+
+    void calculateGalutinis(int input) {
+        if (input == 1) {
+            galutinis = vid;
+        } else {
+            galutinis = med;
+        }
+    }
+
+    void calculateVidurkis() {
         double sum_vid = 0;
-        if (nd.size() == 0) {
+        if (nd.empty()) {
             vid = 0.6 * egzaminas;
         } else {
-            for (int i = 0; i < nd.size(); i++) sum_vid += nd[i];
+            for (const auto& value : nd) {
+                sum_vid += value;
+            }
             vid = ((sum_vid / nd.size()) * 0.4) + (0.6 * egzaminas);
         }
     }
 
-    void Mediana() {
-        if (nd.size() >= 1) {
+    void calculateMediana() {
+        if (!nd.empty()) {
             sort(nd.begin(), nd.end());
             if (nd.size() % 2 == 0) {
-                med = nd[nd.size() / 2 - 1] + nd[nd.size() / 2] / 2;
+                med = (nd[nd.size() / 2 - 1] + nd[nd.size() / 2]) / 2;
             } else {
-                med = nd[(nd.size() / 2)];
+                med = nd[nd.size() / 2];
             }
         } else {
             med = 0;
@@ -52,8 +65,14 @@ struct Duomenys {
         med = 0.4 * med + 0.6 * egzaminas;
     }
 
-
-
-    }
-;
+private:
+    std::string vardas;
+    std::string pavarde;
+    std::vector<double> nd; // namu darbai
+    int egzaminas;
+    float vid;
+    float med;
+    int studentu_kiekis;
+    float galutinis;
+};
 #endif
